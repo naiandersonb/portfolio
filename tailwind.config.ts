6518c09e4b80159f01eba21458de27a5b0adc354
naiandersonb <nayanderson2014@gmail.com>
2023-12-22 08:19:42 -0300

    change settings and tailwind.config'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,6 @@ const config: Config = {
     fontFamily: {
       bebas: 'var(--bebas-neue)',
       inter: 'var(--inter)'
-    },
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      }
     }
   },
   plugins: [],
